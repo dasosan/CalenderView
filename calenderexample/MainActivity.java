b4f008c9f6e5dc467dd69b8a468fb604a1548681
dasosan <89020304+dasosan@users.noreply.github.com>
2021-08-26 23:15:37 +0900

    Add files via upload

---
+++ calenderexample/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.calenderexample;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.CalendarMode;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+
+import java.util.Calendar;
+
+
+public class MainActivity extends AppCompatActivity {
+    MaterialCalendarView materialCalendarView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        materialCalendarView=findViewById(R.id.calendarView);
+        materialCalendarView.state().edit()
+                .setFirstDayOfWeek(Calendar.MONDAY)
+                .setMinimumDate(CalendarDay.from(2017, 4, 3))
+                .setMaximumDate(CalendarDay.from(2030, 11, 31))
+                .setCalendarDisplayMode(CalendarMode.MONTHS)
+                .commit();
+
+        materialCalendarView.addDecorators(
+                new SundayDecorator(),
+                new SaturdayDecorator());
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
